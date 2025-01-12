6725354fe2e02fd2ac7eeb8d55ddfe208d547226
Ankit-Kumar <55393887+Ankitkumadlhi@users.noreply.github.com>
2025-01-12 13:44:05 +0530

    Update settings.py

--- Ecommerce_site/settings.py
+++ Ecommerce_site/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 #Load the .env file
 # load_dotenv()
 
-DB_PASSWORD = os.environ('DB_PASSWORD')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
